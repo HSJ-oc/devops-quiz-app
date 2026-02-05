99fbb7db9ced1c0eb5cfba9d103d242088a237d7
Student B <60302091@udst.edu.qa>
2026-02-05 14:23:56 +0300

    implementing topic selection

--- apps.js
+++ apps.js
@@ -7,3 +7,27 @@ fetch("questions.json")
     console.log("Questions loaded", questions);
   })
   .catch(error => console.error("Error loading questions:", error));
+const topicSelect = document.getElementById("topicSelect");
+
+function loadTopics() {
+  const topics = [...new Set(questions.map(q => q.topic))];
+
+  topics.forEach(topic => {
+    const option = document.createElement("option");
+    option.value = topic;
+    option.textContent = topic;
+    topicSelect.appendChild(option);
+  });
+}
+
+topicSelect.addEventListener("change", () => {
+  displayQuestion();
+});
+
+fetch("questions.json")
+  .then(res => res.json())
+  .then(data => {
+    questions = data;
+    loadTopics();
+    displayQuestion();
+  });
